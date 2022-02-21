09bd7f363ceba8f0bbf0e06f51bb99751efda30e
makaota <74915165+makaota@users.noreply.github.com>
2022-02-21 18:51:08 +0200

    The second part of this project is completed and we need to run the third part. which we will see how to use arguments in a saver way, meaning we are going to use saveArgs instead of arguments which is powerful and useful and is using Jetpack approach

--- app/src/main/java/com/makaota/navdemo/EnterDetailsFragment.kt
+++ app/src/main/java/com/makaota/navdemo/EnterDetailsFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 
 
 class EnterDetailsFragment : Fragment() {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +27,37 @@ class EnterDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_enter_details, container, false)
+
+        val etName = rootView.findViewById<EditText>(R.id.et_name)
+        val etMobileNumber = rootView.findViewById<EditText>(R.id.et_mobile)
+
+
         val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
 
         btnVerifyDetails.setOnClickListener {
 
-            findNavController().navigate(R.id.action_enterDetailsFragment4_to_verifyDetailsFragment2)
+            val firstName = etName.text.toString()
+            val mobile = etMobileNumber.text.toString()
+
+            when{
+                firstName.isEmpty()->{
+                    Toast.makeText(activity,"Enter name",Toast.LENGTH_SHORT).show()
+                }
+                mobile.isEmpty()->{
+                    Toast.makeText(activity,"Enter mobile number",Toast.LENGTH_SHORT).show()
+                }
+                else->{
+                    val bundle = bundleOf(
+                        "name" to firstName,
+                        "mobile" to mobile.toLong()
+                    )
+                    findNavController()
+                        .navigate(R.id.action_enterDetailsFragment4_to_verifyDetailsFragment2,
+                        bundle)
+                }
+            }
+
+
         }
 
         return rootView
--- app/src/main/java/com/makaota/navdemo/VerifyDetailsFragment.kt
+++ app/src/main/java/com/makaota/navdemo/VerifyDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class VerifyDetailsFragment : Fragment() {
@@ -18,8 +19,20 @@ class VerifyDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val name = arguments?.getString("name")
+        val mobileNumber = arguments?.getLong("mobile")
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verify_details, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_verify_details, container, false)
+
+        val tvName = rootView.findViewById<TextView>(R.id.tv_name)
+        val tvMobile = rootView.findViewById<TextView>(R.id.tv_mobile_number)
+
+        tvName.text = name
+        tvMobile.text = mobileNumber.toString()
+
+        return rootView
     }
 
 
